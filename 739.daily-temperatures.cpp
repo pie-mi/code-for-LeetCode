a1d4c9da66b009246e380c5b2702989d63484c4c
pie-mi <1719487778@qq.com>
2025-02-05 22:10:00 +0800

    simplify Leetcode 739

--- 739.daily-temperatures.cpp
+++ 739.daily-temperatures.cpp
@@ -3,41 +3,36 @@
  *
  * [739] Daily Temperatures
  */
-#include <vector>
 #include <stack>
 #include <utility>
+#include <vector>
 using namespace std;
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    vector<int> dailyTemperatures(vector<int>& temperatures) {
-        //看了栈这个标签进来的
-        //当前最高入栈于栈顶，有更高的就弹走
-        //若中间有低的，小于栈顶就入栈，又有高于栈顶的就不断弹走与新栈顶比较
-        
-        stack<pair<int,int>> higerTemperatures; //first是温度，second是下标
-        vector<int> result(temperatures.size(), 0); //AI补全，clever，中间的最高温要到末尾才知道没有更高的，初始化先给个0，不用后面再如何找回对应下标填0
-        higerTemperatures.push(make_pair(temperatures[0],0)); //题目说至少有一个，非空
+    vector<int> dailyTemperatures(vector<int> &temperatures)
+    {
+        // 看了栈这个标签进来的
+        // 当前最高入栈于栈顶，有更高的就弹走
+        // 若中间有低的，小于栈顶就入栈，又有高于栈顶的就不断弹走与新栈顶比较
+
+        stack<pair<int, int>> higerTemperatures;               // first是温度，second是下标
+        vector<int> result(temperatures.size(), 0);            // AI补全，clever，中间的最高温要到末尾才知道没有更高的，初始化先给个0，不用后面再如何找回对应下标填0
+        higerTemperatures.push(make_pair(temperatures[0], 0)); // 题目说至少有一个，非空
         for (auto i = 1; i < temperatures.size(); i++)
         {
-            if (!higerTemperatures.empty() && temperatures[i] > higerTemperatures.top().first)
+
+            while (!higerTemperatures.empty() && temperatures[i] > higerTemperatures.top().first)
             {
-                while (!higerTemperatures.empty() && temperatures[i] > higerTemperatures.top().first)
-                {
-                    int index = higerTemperatures.top().second;
-                    result[index] = i - index; //AI 下标差就是天数
-                    higerTemperatures.pop();
-                }
-                higerTemperatures.push(make_pair(temperatures[i], i)); //将自己入栈
+                int index = higerTemperatures.top().second;
+                result[index] = i - index; // AI 下标差就是天数
+                higerTemperatures.pop();
             }
-            else
-            {
-                higerTemperatures.push(make_pair(temperatures[i], i)); //AI 入栈，先比较再决定是否入栈
-            }
-            
+
+            higerTemperatures.push(make_pair(temperatures[i], i)); // AI 入栈，先比较再决定是否入栈，自己也入栈
         }
         return result;
     }
 };
 // @lc code=end
-
